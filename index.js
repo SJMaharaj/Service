a5b6363207514f5ae775f78286d1f8d5d3ca4b73
Sonal Maharaj <sonal.maharaj@accenture.com>
2023-08-29 15:28:51 +0200

    cors update

--- index.js
+++ index.js
@@ -12,32 +12,39 @@ const app = express();
 dotenv.config();
 
 const connect = () => {
-    mongoose.connect(process.env.MONGO).then(()=>{
+    mongoose.connect(process.env.MONGO).then(() => {
         console.log("Connected to DB");
     })
-    .catch((err)=>{
-        throw err;
-    });
+        .catch((err) => {
+            throw err;
+        });
 };
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(cookieParser());
 app.use(express.json());
-app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes);
-app.use("/api/videos",videoRoutes);
-app.use("/api/comments",commentRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
 
-app.use((err,req,res,next)=>{
+
+app.use((err, req, res, next) => {
     const status = err.status || 500;
     const message = err.message || "Something went wrong";
     return res.status(status).json({
-        success:false,
-        status:status,
-        message:message
+        success: false,
+        status: status,
+        message: message
     })
 });
 
-app.listen(8800,()=>{
+app.listen(8800, () => {
     connect();
     console.log("Connected to Server");
 })
